50ae9e97487b9033e08c454f20635d8fca2334bc
Ygor Oliveira <ygoliveira5@gmail.com>
2021-05-03 18:31:53 -0300

    Basic static component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Navbar from "components/Navbar";
+import Footer from "components/Navbar/Footer";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
